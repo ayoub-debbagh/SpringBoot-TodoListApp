30498a846b4e206eb7c8852c8401f0db641065a2
Ayoub Debbagh <ayoubdebbagh7@gmail.com>
2024-12-11 16:57:05 +0100

    Task model

---
+++ src/main/java/com/example/spring_boot_todolist/models/Task.java
@@ -0,0 +1,40 @@
+package com.example.spring_boot_todolist.models;
+
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+@Entity
+@Table(name = "tasks")
+public class Task {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String title;
+
+    private String description;
+
+    private boolean completed;
+
+    private LocalDateTime createdAt;
+
+    private LocalDateTime updatedAt;
+
+    @PrePersist
+    public void prePersist() {
+        createdAt = LocalDateTime.now();
+        updatedAt = LocalDateTime.now();
+    }
+
+}

e162d974be920d1b44141ce71257a976c2a3c207
Ayoub Debbagh <ayoubdebbagh7@gmail.com>
2024-12-11 16:35:49 +0100

    initializing - dependencies + application.properties

---
+++ src/main/java/com/example/spring_boot_todolist/SpringBootTodolistApplication.java
@@ -0,0 +1,13 @@
+package com.example.spring_boot_todolist;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootTodolistApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootTodolistApplication.class, args);
+	}
+
+}

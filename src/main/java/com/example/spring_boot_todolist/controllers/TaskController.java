421a1d84dedfe57f212f56a839bf3ad585f0c990
Ayoub Debbagh <ayoubdebbagh7@gmail.com>
2024-12-11 17:18:15 +0100

    task controller

---
+++ src/main/java/com/example/spring_boot_todolist/controllers/TaskController.java
@@ -0,0 +1,42 @@
+package com.example.spring_boot_todolist.controllers;
+
+
+import com.example.spring_boot_todolist.models.Task;
+import com.example.spring_boot_todolist.services.TaskService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
+
+@Controller
+@RequestMapping("/tasks")
+public class TaskController {
+
+    @Autowired
+    private TaskService taskService;
+
+    @GetMapping
+    public String getTasks(Model model) {
+        model.addAttribute("tasks", taskService.getAllTasks());
+        return "task-list";
+    }
+
+    @GetMapping("/new")
+    public String newTask(Model model) {
+        model.addAttribute("task", new Task());
+        return "task-form";
+    }
+
+    @PostMapping
+    public String saveTask(@ModelAttribute Task task) {
+        taskService.saveTask(task);
+        return "redirect:/tasks";
+    }
+
+    @GetMapping("/delete/{id}")
+    public String deleteTask(@PathVariable Long id) {
+        taskService.deleteTask(id);
+        return "redirect:/tasks";
+    }
+
+}
